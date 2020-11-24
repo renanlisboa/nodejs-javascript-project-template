2b455019603670d11692de82bc0c679284b63483
Renan Lisboa <renlp89.developer@gmail.com>
2020-11-24 16:21:09 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'Rnan/2020',
+  database: 'gobarber_js',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
